b9b8ab2f3cd689d337d20a94693dde51a020baac
Ben Graham <grahamben7@gmail.com>
2024-03-03 23:16:50 +0200

    Update Server

--- logger.js
+++ logger.js
@@ -12,34 +12,7 @@ const logger = createLogger({
     transports: [
         new transports.Console({level: 'silly'}),
         new transports.File({filename: 'server.log', level: 'silly'}),
-        // new transports.DailyRotateFile({
-        //     filename: 'server-%Date%/log',
-        //     datePattern: "DD-MM-YY",
-        //     zippedArchieved: false,
-        //     maxFiles: '7d'
-        // })
-
     ]
 })
 
 module.exports = logger
-
-// const logger = createLogger({
-//     format: customFormat,
-//     transports: [
-//       new transports.Console({ level: 'silly' }),
-//       new transports.File({
-//         filename: 'server.log',
-//         level: 'silly',
-//       }),
-//       new transports.DailyRotateFile({
-//         filename: 'server-%DATE%.log',
-//         datePattern: 'YYYY-MM-DD',
-//         zippedArchive: true,
-//         maxSize: '20m',
-//         maxFiles: '14d', // Keep logs for the last 14 days
-//       }),
-//     ],
-//   });
-  
-//   module.exports = logger;
